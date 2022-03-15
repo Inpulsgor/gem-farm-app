c0c1ce74125cfaf7e165da4eae95ffc9a7cafcd2
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2022-03-15 22:13:35 +0200

    updated mobile FAQs

--- app/gem-farm/src/views/Farmer.vue
+++ app/gem-farm/src/views/Farmer.vue
@@ -175,7 +175,6 @@
       <div class="hidden md:flex w-full md:w-1/2 mb-12 md:mb-0">
         <div class="faq flex flex-col p-6 w-full rounded-lg">
           <span class="text-base text-white mb-5">How to stack?</span>
-          <span class="text-base mb-4">Stacking instructions goes here</span>
 
           <ul class="mb-4">
             <li class="text-sm mb-3">
@@ -238,72 +237,74 @@
         </div>
       </div>
 
-      <div class="w-full md:hidden">
+      <div class="w-full md:hidden mb-6 accord rounded-lg">
         <input type="checkbox" name="panel" id="panel-1" class="hidden" />
-        <label for="panel-1" class="relative block text-white p-4 border-grey"
+        <label for="panel-1" class="relative block text-base text-white p-4 m-0"
           >How to stack?</label
         >
-        <div class="accordion__content overflow-hidden bg-grey-lighter">
-          <span class="text-base mb-4">Stacking instructions goes here</span>
 
-          <ul class="mb-4">
-            <li class="text-sm mb-3">
-              1. Make sure you are using the wallet which contains your DH NFT
-            </li>
-            <li class="text-sm mb-3">
-              2. Open your browser and go to the staking link
-            </li>
-            <li class="text-sm mb-3">
-              3. In the bar at bottom please select the wallet you are using and
-              connect
-            </li>
-            <li class="text-sm mb-3 walletOverflow">
-              4. In the address bar please input
-              8EV1K3kWmq2hbRtQfnBg3wbvELEorJajbyJhRGwVptwj and click the new
-              farmer button
-            </li>
-            <li class="text-sm mb-3">
-              5. This will allow you to start staking
-            </li>
-            <li class="text-sm mb-3">
-              6. Once the transaction goes through - the page will autopopulate
-            </li>
-            <li class="text-sm mb-3">
-              7. Select you DH NFT by clicking on it and move it to the vault by
-              clicking the arrow
-            </li>
-            <li class="text-sm mb-3">
-              8. Click the green 'Move to Vault' button that has appeared and
-              approve the transaction
-            </li>
-            <li class="text-sm mb-3">
-              9. Once the NFT has moved to the vault select it and now click the
-              begin staking button
-            </li>
-            <li class="text-sm mb-3">
-              10. You will see your status update change to say 'staked'
-            </li>
-            <li class="text-sm mb-3">
-              11. All done! Come back later and follow the process above - and
-              then scroll down and click the Refresh account button to see how
-              many tokens you've received!
-            </li>
-            <li class="text-sm mb-3">
-              12. Once you've refreshed the account you'll see the claim token
-              button update with the number of tokens you can claim
-            </li>
-            <li class="text-sm">
-              13. Go ahead and claim those tokens! Make sure to come back to
-              claim your rewards weekly
-            </li>
-          </ul>
-          <iframe
-            class="videoFrame"
-            title="Inline Frame Example"
-            allowFullScreen
-            src="https://www.youtube.com/embed/7B9krM5zQCw"
-          >
-          </iframe>
+        <div class="accordion__content overflow-hidden">
+          <div class="faqMobile flex flex-col p-6 w-full rounded-lg">
+            <ul class="mb-4">
+              <li class="text-sm mb-3">
+                1. Make sure you are using the wallet which contains your DH NFT
+              </li>
+              <li class="text-sm mb-3">
+                2. Open your browser and go to the staking link
+              </li>
+              <li class="text-sm mb-3">
+                3. In the bar at bottom please select the wallet you are using
+                and connect
+              </li>
+              <li class="text-sm mb-3 walletOverflow">
+                4. In the address bar please input
+                8EV1K3kWmq2hbRtQfnBg3wbvELEorJajbyJhRGwVptwj and click the new
+                farmer button
+              </li>
+              <li class="text-sm mb-3">
+                5. This will allow you to start staking
+              </li>
+              <li class="text-sm mb-3">
+                6. Once the transaction goes through - the page will
+                autopopulate
+              </li>
+              <li class="text-sm mb-3">
+                7. Select you DH NFT by clicking on it and move it to the vault
+                by clicking the arrow
+              </li>
+              <li class="text-sm mb-3">
+                8. Click the green 'Move to Vault' button that has appeared and
+                approve the transaction
+              </li>
+              <li class="text-sm mb-3">
+                9. Once the NFT has moved to the vault select it and now click
+                the begin staking button
+              </li>
+              <li class="text-sm mb-3">
+                10. You will see your status update change to say 'staked'
+              </li>
+              <li class="text-sm mb-3">
+                11. All done! Come back later and follow the process above - and
+                then scroll down and click the Refresh account button to see how
+                many tokens you've received!
+              </li>
+              <li class="text-sm mb-3">
+                12. Once you've refreshed the account you'll see the claim token
+                button update with the number of tokens you can claim
+              </li>
+              <li class="text-sm">
+                13. Go ahead and claim those tokens! Make sure to come back to
+                claim your rewards weekly
+              </li>
+            </ul>
+            <iframe
+              class="videoFrame"
+              title="Inline Frame Example"
+              allowFullScreen
+              src="https://www.youtube.com/embed/7B9krM5zQCw"
+            >
+            </iframe>
+          </div>
         </div>
       </div>
     </div>
@@ -734,6 +735,11 @@ export default defineComponent({
   max-height: 56px;
 }
 
+.faqMobile {
+  height: max-content;
+  color: #a1a1a1;
+}
+
 .faq {
   border: 1px solid #1d1d1d;
   height: max-content;
@@ -772,6 +778,10 @@ input:checked + label:after {
 }
 input[name='panel']:checked ~ .accordion__content {
   /* Get this as close to what height you expect */
-  max-height: 50em;
+  max-height: min-content;
+}
+
+.accord {
+  border: 1px solid #1d1d1d;
 }
 </style>
